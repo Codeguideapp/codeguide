5860c53a01b44b5792e5e9c67f7dbd3441e018ff
domagoj <domagoj.kriskovic@gmail.com>
2021-08-18 13:21:44 +0200

    monaco editor amd version

---
+++ src/Editor.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react'
+
+export function Editor() {
+  const editorDiv = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (editorDiv.current) {
+      const editor = window.monaco.editor.create(editorDiv.current, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+          '\n'
+        ),
+        language: 'javascript',
+      })
+      console.log(editor)
+    }
+  }, [editorDiv])
+
+  return (
+    <div
+      ref={editorDiv}
+      id="editor"
+      style={{
+        width: 800,
+        height: 600,
+      }}
+    ></div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Editor } from './Editor'
+import reportWebVitals from './reportWebVitals'
+import type * as monaco from 'monaco-editor'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+declare global {
+  interface Window {
+    monacoLoaded: boolean
+    monaco: typeof monaco
+  }
+}
+
+const renderApp = () =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <div>
+        <Editor />
+        <App />
+      </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+
+// to avoid dealing with monaco and webpack I am using amd version
+// taken from https://github.com/microsoft/monaco-editor-samples/blob/main/browser-amd-editor/index.html
+// monaco is loaded in index.html and monacoReady event is triggered when loaded
+if (window.monacoLoaded) {
+  renderApp()
+} else {
+  document.addEventListener('monacoReady', renderApp)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
