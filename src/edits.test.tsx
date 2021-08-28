bcb5e35694505525e087c11aeddfcbd6f1baf202
domagoj <domagoj.kriskovic@gmail.com>
2021-08-28 20:59:30 +0200

    create changes from diff

--- src/edits.test.tsx
+++ src/edits.test.tsx
@@ -1,26 +1,31 @@
-import { createYText, sync } from "./edits";
-import * as diff from "diff";
+import {
+  createCommands,
+  createYText,
+  diff,
+  executeCommands,
+  sync,
+} from './edits';
 
-test("should apply independent edits", () => {
-  const initial = createYText("example text");
-  expect(initial.toString()).toBe("example text");
+test('should apply independent edits', () => {
+  const initial = createYText('example text');
+  expect(initial.toString()).toBe('example text');
 
   const text1 = createYText(initial);
   const text2 = createYText(initial);
 
-  text1.insert(0, "1");
-  text2.insert(8, "2");
+  text1.insert(0, '1');
+  text2.insert(8, '2');
 
-  expect(text1.toString()).toBe("1example text");
-  expect(text2.toString()).toBe("example 2text");
+  expect(text1.toString()).toBe('1example text');
+  expect(text2.toString()).toBe('example 2text');
 
   sync(text1, text2);
 
-  expect(text1.toString()).toBe("1example 2text");
-  expect(text2.toString()).toBe("1example 2text");
+  expect(text1.toString()).toBe('1example 2text');
+  expect(text2.toString()).toBe('1example 2text');
 });
 
-test("should diff", () => {
+test('should diff', () => {
   const oldStr = `const renderApp = () =>
   ReactDOM.render(
     <React.StrictMode>
@@ -42,51 +47,9 @@ test("should diff", () => {
   )
   // new line`;
 
-  const diffs = diff.diffWords(oldStr, newStr) as (Diff.Change & {
-    _skip?: boolean;
-    replace?: string;
-  })[];
+  const changes = diff(oldStr, newStr);
+  const commands = createCommands(changes);
+  const res = executeCommands(commands, oldStr);
 
-  const diffsWithReplace = diffs
-    .map((diff, index, all) => {
-      if (diff._skip) {
-        return false;
-      }
-
-      if (diff.removed && all[index + 1]?.added) {
-        all[index + 1]._skip = true;
-
-        return {
-          removed: true,
-          replace: all[index + 1].value,
-          value: diff.value,
-        };
-      }
-
-      return diff;
-    })
-    .filter((val) => !!val);
-
-  // dodat move diff (pregledat ima li istih valuea jedan add drugi remove)
-  // to uvijek ide skupa isto kao i replace
-
-  // calc index za commandu
-  // gleda se samo prethodni value.length i ignorira se added
-
-  // dobije se index komande, za add, delete i replace (koji se sastoji od add del)
-  // na orig tekst
-
-  // od toga se rade yjs komande i docs
-  // i synca
-
-  // rez mora bit ko newdoc
-
-  // let i = 0;
-  // for (const diff of diffs) {
-  //   if () {
-  //     i;
-  //   }
-
-  //   i++;
-  // }
+  expect(res.toString()).toBe(newStr);
 });
--- src/edits.ts
+++ src/edits.ts
@@ -1,11 +1,135 @@
-import * as Y from "yjs";
+import * as Y from 'yjs';
+import * as diffLib from 'diff';
+
+export type Command =
+  | {
+      type: 'insert';
+      value: string;
+      index: number;
+    }
+  | {
+      type: 'replace';
+      value: string;
+      oldValue: string;
+      index: number;
+    }
+  | {
+      type: 'delete';
+      index: number;
+      delLength: number;
+    };
+
+export type Change = {
+  count?: number;
+  value: string;
+  added?: boolean;
+  removed?: boolean | undefined;
+  replace?: string;
+};
+
+export function diff(oldVal: string, newVal: string): Change[] {
+  const diffs = diffLib.diffWords(oldVal, newVal) as (Change & {
+    _skip?: boolean;
+  })[];
+
+  const diffsWithReplace = diffs.map((diff, index, all) => {
+    if (diff._skip) {
+      return false;
+    }
+
+    if (diff.removed && all[index + 1]?.added) {
+      all[index + 1]._skip = true;
+
+      return {
+        removed: true,
+        replace: all[index + 1].value,
+        value: diff.value,
+      };
+    }
+
+    return diff;
+  });
+
+  const changes: Change[] = [];
+  diffsWithReplace.forEach((val) => {
+    if (val) {
+      changes.push(val);
+    }
+  });
+
+  return changes;
+}
+
+export function createCommands(changes: Change[]): Command[] {
+  const commands: Command[] = [];
+  let index = 0;
+
+  for (const change of changes) {
+    if (!change) continue;
+
+    if (change.added) {
+      commands.push({
+        type: 'insert',
+        value: change.value,
+        index,
+      });
+    } else if (change.removed) {
+      if (change.replace) {
+        commands.push({
+          type: 'replace',
+          index,
+          oldValue: change.value,
+          value: change.replace,
+        });
+      } else {
+        commands.push({
+          type: 'delete',
+          index,
+          delLength: change.value.length,
+        });
+      }
+
+      index += change.value.length;
+    } else {
+      index += change.value.length;
+    }
+  }
+
+  return commands;
+}
+
+export function executeCommands(commands: Command[], initialValue: string) {
+  const initial = createYText(initialValue);
+  const targetYText = createYText(initial);
+
+  for (const command of commands) {
+    const changeYText = createYText(initial);
+
+    switch (command.type) {
+      case 'insert':
+        changeYText.insert(command.index, command.value);
+        break;
+      case 'replace':
+        changeYText.delete(command.index, command.oldValue.length);
+        changeYText.insert(command.index, command.value);
+        break;
+      case 'delete':
+        changeYText.delete(command.index, command.delLength);
+        break;
+    }
+
+    sync(targetYText, changeYText);
+  }
+
+  return targetYText;
+}
 
 export function sync(ytext1: Y.Text, ytext2: Y.Text) {
   const ydoc1 = ytext1.doc;
   const ydoc2 = ytext2.doc;
 
   if (!ydoc1 || !ydoc2) {
-    throw new Error("missing y.doc instance");
+    throw new Error('missing y.doc instance');
   }
   const state1 = Y.encodeStateAsUpdate(ydoc1);
   const state2 = Y.encodeStateAsUpdate(ydoc2);
@@ -16,9 +140,9 @@ export function sync(ytext1: Y.Text, ytext2: Y.Text) {
 
 export const createYText = (initial: Y.Text | string) => {
   const doc = new Y.Doc();
-  const ytext = doc.getText("file");
+  const ytext = doc.getText('file');
 
-  if (typeof initial === "string") {
+  if (typeof initial === 'string') {
     ytext.insert(0, initial);
   } else {
     sync(ytext, initial);
