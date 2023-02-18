c9ee4199b80a5f35bcca2963098bdbc6975f4195
dom <domagoj.kriskovic@gmail.com>
2023-02-18 12:48:31 +0100

    fix: fontawsome css bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,17 @@ import '../styles/globals.css';
 import '../components/Steps/Steps.css';
 import '../components/BottomBar/BottomBar.css';
 import 'highlight.js/styles/github-dark.css';
-
-import { api } from '../utils/api';
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+
+import { api } from '../utils/api';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
